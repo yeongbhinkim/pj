ecedb519f5dcfe24d6b84edde0d1aeb0489f2024
kimYeongBhin <abc282v@naver.com>
2022-04-13 00:45:11 +0900

    미정

---
+++ cafesurfer/src/main/java/com/kh/cafesurfer/web/form/memberShip/FindEmailAndPasswdForm.java
@@ -0,0 +1,11 @@
+package com.kh.cafesurfer.web.form.memberShip;
+
+import lombok.Data;
+
+@Data
+public class FindEmailAndPasswdForm {
+  String memberEmail;
+  String memberName;
+  String memberTel;
+  String memberPasswd;
+}
